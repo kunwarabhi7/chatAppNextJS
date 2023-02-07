1593b8ded4003f044e30ee4baa9c6cafc32e03dc
Abhishek <abhishekparmar162@gmail.com>
2023-02-07 19:03:49 +0530

    setup firebase

---
+++ utils/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCSABlcGQcR2fP0hs8SsSuakevz_6XtBck",
+  authDomain: "chat-d3499.firebaseapp.com",
+  projectId: "chat-d3499",
+  storageBucket: "chat-d3499.appspot.com",
+  messagingSenderId: "960367213244",
+  appId: "1:960367213244:web:33a2c86258cf38ac5faeb9"
+};
+
+const app = initializeApp(firebaseConfig);

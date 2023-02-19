3e73619bef68b7a008e183fbe2d697e17cdb1765
Abhishek <abhishekparmar162@gmail.com>
2023-02-19 21:24:25 +0530

    install react-hooks

--- pages/index.js
+++ pages/index.js
@@ -5,14 +5,16 @@ import {auth} from '../utils/firebase'
 
 
 const Home = () => {
-  const [user] = useAuthState(auth)
 
   return (
-    <div className='flex justify-end'>
+    <div className='flex justify-around'>
       <h1>Chat App </h1>
       <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Log Out</button>
     </div>
   )
 }
+
+
+
 
 export default Home

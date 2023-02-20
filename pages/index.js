9525bf9f936db5919206b4b7def402f88294e141
Abhishek <abhishekparmar162@gmail.com>
2023-02-20 16:19:23 +0530

    get an error

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import ChatInput from '@/components/ChatInput';
 import Login from '@/components/Login'
 import { useState } from 'react';
 import {useAuthState} from 'react-firebase-hooks/auth'
@@ -13,13 +14,15 @@ const [chatRoom, setChatRoom] = useState(null)
     <>
  
      <div className="dark:bg-gray-900 text-white justify-end flex ">
-     <button class="relative inline-flex items-center justify-center mt-4 p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
-  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+     <button className="relative inline-flex items-center justify-center mt-4 p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+  <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
       LogOut
   </span>
 </button>
      </div>
-          {chatRoom ? Chat : "Enter Your Room Name"}
+          {chatRoom ? "Chat" : (
+            <ChatInput chatRoom={chatRoom} setChatRoom={setChatRoom} />
+          )}
     </>
   )
 }

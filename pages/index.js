a392683195ca104af82d8a6cf8a8a91572d7f7e6
Abhishek <abhishekparmar162@gmail.com>
2023-02-20 15:45:39 +0530

    added chatroom

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,25 @@
-import { useRouter } from 'next/router'
-import React from 'react'
+import Login from '@/components/Login'
+import { useState } from 'react';
 import {useAuthState} from 'react-firebase-hooks/auth'
 import {auth} from '../utils/firebase'
 
-
-const Home = () => {
-
+export default function Home() {
+  const [user] = useAuthState(auth);
+const [chatRoom, setChatRoom] = useState(null)
+  if(!user) return <Login />
+ 
+ 
   return (
-    <div className='flex justify-around'>
-      <h1>Chat App </h1>
-      <button type="button" className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Log Out</button>
-    </div>
+    <>
+ 
+     <div className="dark:bg-gray-900 text-white justify-end flex ">
+     <button class="relative inline-flex items-center justify-center mt-4 p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+  <span class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+      LogOut
+  </span>
+</button>
+     </div>
+          {chatRoom ? Chat : "Enter Your Room Name"}
+    </>
   )
 }
-
-
-
-
-export default Home

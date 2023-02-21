b45138c44b62002e6a01f230f41a5913981d625d
Abhishek <abhishekparmar162@gmail.com>
2023-02-21 12:43:37 +0530

    setup signOut

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
+import Chat from '@/components/Chat';
 import ChatInput from '@/components/ChatInput';
 import Login from '@/components/Login'
+import { signOut } from 'firebase/auth';
 import { useState } from 'react';
 import {useAuthState} from 'react-firebase-hooks/auth'
 import {auth} from '../utils/firebase'
@@ -8,6 +10,14 @@ export default function Home() {
   const [user] = useAuthState(auth);
 const [chatRoom, setChatRoom] = useState(null)
 
+const SignOut = () => {
+  signOut(auth).then(()=>{
+    console.log('Sign Out')
+  }).catch((err)=>{
+    console.log('Error');
+  })
+}
+
   if(!user) return <Login />
  
  
@@ -15,13 +25,13 @@ const [chatRoom, setChatRoom] = useState(null)
     <>
  
      <div className="dark:bg-gray-900 text-white justify-end flex ">
-     <button className="relative inline-flex items-center justify-center mt-4 p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+     <button onClick={SignOut} className="relative inline-flex items-center justify-center mt-4 p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
   <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
       LogOut
   </span>
 </button>
      </div>
-          {chatRoom ? "Chat" : (
+          {<Chat /> ? "Chat" : (
             <ChatInput setChatRoom={setChatRoom}  />
           )}
     </>

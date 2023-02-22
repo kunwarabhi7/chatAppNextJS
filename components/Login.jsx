facd52e946557802aa153da2f1bb5ab584aa27b6
Abhishek <abhishekparmar162@gmail.com>
2023-02-22 13:51:05 +0530

    styling

--- components/Login.jsx
+++ components/Login.jsx
@@ -21,12 +21,12 @@ const Login = () => {
 
   return (
     <div className="flex items-center justify-center h-screen">
-      <div className="bg-white p-6 rounded-lg shadow-xl">
+      <div className="p-6 rounded-lg shadow-xl">
         <div className="mb-4 flex items-center justify-center">
         
 
           <button
-            className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-full"
+            className=" hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded-full"
             onClick={handleGoogleLogin}
             >
                 <div className='flex'>
